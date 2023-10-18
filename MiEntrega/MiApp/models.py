882bc27e091bee02ae22345b9b0c819ba373e3fa
javi19632003 <javi19632003@gmail.com>
2023-10-18 16:51:24 -0300

    armaqndo avatar

--- MiEntrega/MiApp/models.py
+++ MiEntrega/MiApp/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db                  import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Productos(models.Model):
@@ -16,6 +17,13 @@ class Clientes(models.Model):
     
     def __str__(self):
         return f"{self.id} - {self.nombre}"
+
+class User1(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank = True)
+    def __str__(self):
+        return f"{self.user} - {self.imagen}"
+
     
 class Ordenes(models.Model):
     nroord    = models.IntegerField()
